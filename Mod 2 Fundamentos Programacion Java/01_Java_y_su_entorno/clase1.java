1a920745c411376653090628357a1e9df2fcb52b
Leandroaraya <araya.leandro@gmail.com>
2026-05-14 23:03:33 -0400

    entorno java

---
+++ Mod 2 Fundamentos Programacion Java/01_Java_y_su_entorno/clase1.java
@@ -0,0 +1,17 @@
+/*
+flujo de trabajo: Codigo escrito en java> se compila haciendolo el codigo java en bytecode>> luego lo toma el JVM para ejecutarlo
+
+JVM-> java virtual machine: quien ejecuta lo programado
+JRE-> Java Runtime Environment: es lo necesario para ejecutar el codigo. contiene JVM y librerias
+JDK-> Java Development Kit: contiene JRE y herramientas de desarrollo
+
+Estructura base parte de una clase. ideal llamarla Main con el mismo nombre del archivo
+en vscode hay que intalar java y las extensiones previamente
+
+*/
+public class clase1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mund00o");
+        System.out.println("holamundo");
+    }
+}
